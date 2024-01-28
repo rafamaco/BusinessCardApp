90b33710a027890e5c389780c9b090f08ad8bdc8
Rafael Magalhaes <rafamaco@gmail.com>
2024-01-28 16:16:51 -0700

    Initial commit

---
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.example.businesscard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusinessCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeQuadrantApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BusinessCardTheme {
+        ComposeQuadrantApp()
+    }
+}
+
+@Composable
+fun ComposeQuadrantApp() {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .background(Color.Green)
+    ) {
+        Row (
+            Modifier
+                .weight(2.5f)
+        ){
+            ComposableImageCard(
+                title = "Android Developer Extraordinarie",
+                fullName = "Jennifer Doe",
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row (Modifier.weight(1f)){
+            ComposableInfoCard(
+                phone = "+00(00)000-0000",
+                socialMedia = "@socialMediaHandle",
+                email = "email@doemail.com",
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ComposableImageCard(
+    title: String,
+    fullName: String,
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.android_logo) ,
+            contentDescription = null,
+            modifier = Modifier
+                .size(150.dp)
+                .background(Color.DarkGray)
+        )
+        Text(
+            text = fullName,
+            textAlign = TextAlign.Justify,
+            fontSize = 50.sp
+        )
+        Text(
+            text = title,
+            textAlign = TextAlign.Justify,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+private fun ComposableInfoCard(
+    phone: String,
+    socialMedia: String,
+    email: String,
+    modifier: Modifier = Modifier
+){
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = phone,
+            textAlign = TextAlign.Justify,
+            fontSize = 20.sp
+        )
+        Text(
+            text = socialMedia,
+            textAlign = TextAlign.Justify,
+            fontSize = 20.sp
+        )
+        Text(
+            text = email,
+            textAlign = TextAlign.Justify,
+            fontSize = 20.sp
+        )
+
+    }
+}
+
